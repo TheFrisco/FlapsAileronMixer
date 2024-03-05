1ede2d42ff66e721866e60071eaf2e85b965fbbf
Robert Schmid <robert@theschmids.org>
2024-03-05 00:12:54 -1000

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+// Roberts First Arduino ... Not Quite "Hello World!" LOL
+#include <Arduino.h>
+
+int led = 13;
+int onDelay = 500;
+int offDelay = 200;
+
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(led, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(led, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(onDelay);           // wait for a second
+  digitalWrite(led, LOW);   // turn the LED off by making the voltage LOW
+  delay(offDelay);          // wait for a second
+}
